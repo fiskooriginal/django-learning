4d9bfb91fb35c857da45ff3e90910920ba4b1ef5
fiskooriginal <fiskowood@gmail.com>
2023-11-30 15:28:22 +0300

    return back to previous project name
    ...

--- mySite/users/views.py
+++ mySite/users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.views.generic import View
 from django.contrib.auth import login, authenticate
 
-from myproject.users.forms import UserCreationForm
+from mySite.users.forms import UserCreationForm
 
 
 class Register(View):
