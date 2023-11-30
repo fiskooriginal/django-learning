b9da463da56f1ab6033964f69a7f295d2a840bea
fiskooriginal <fiskowood@gmail.com>
2023-11-30 13:18:24 +0300

    created login page

--- mySite/blog/views.py
+++ mySite/blog/views.py
@@ -4,20 +4,8 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 
-# def post_list(request):
-#     object_list = Post.objects.filter(status=Post.PostStatus.PUBLISHED)
-#     paginator = Paginator(object_list, 3)
-#     page = request.GET.get('page')
-#     try:
-#         posts = paginator.page('page')
-#     except PageNotAnInteger:
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         posts = paginator.page(paginator.num_pages)
-#     return render(request, 'blog/post/list.html', {
-#         'posts': posts,
-#         'page': page
-#     })
+
+# Create your views here.
 
 class PostListView(ListView):
     queryset = Post.objects.filter(status=Post.PostStatus.PUBLISHED)
@@ -25,13 +13,31 @@ class PostListView(ListView):
     paginate_by = 3
     template_name = 'blog/post/list.html'
 
+
+# def post_list(request):
+#     object_list = Post.objects.filter(status=Post.PostStatus.PUBLISHED)
+#     paginator = Paginator(object_list, 3)
+#     page = request.GET.get('page')
+#     try:
+#         posts = paginator.page(page)
+#     except PageNotAnInteger:
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         posts = paginator.page(paginator.num_pages)
+#
+#     return render(request, 'blog/post/list.html', {
+#         'page': page,
+#         'posts': posts})
+
+
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
                              status='published',
-                             publish_year=year,
-                             publish_month=month,
-                             publish_day=day)
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
+
 
 def post_shar(request, post_id):
     post = get_object_or_404(Post, id=post_id, status='published')
@@ -41,5 +47,5 @@ def post_shar(request, post_id):
             cd = form.cleaned_data
     else:
         form = EmailPostForm()
-    return render(request, "blog/post/share.html", {'post': post,
+    return render(request, 'blog/post/share.html', {'post': post,
                                                     'form': form})
