8bf9d168c31139a9da264e7578dd3a9643bd83d2
fiskooriginal <fiskowood@gmail.com>
2023-11-30 12:25:51 +0300

    fixed import

--- mySite/blog/models.py
+++ mySite/blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.core.urlresolvers import reverse
+from django.shortcuts import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
